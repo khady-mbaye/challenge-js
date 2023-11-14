a3056e199e01fe291ce4b6e64dc68d3c906b25cd
khady-mbaye~ <khadymbaye541@gmail.com>
2023-11-14 14:05:12 +0100

    day 1 1

---
+++ tableau.js
@@ -0,0 +1,15 @@
+var fruits = ['babana' , 'pomme' , 'mangue' , 'orange']
+console.log(fruits)
+
+console.log('')
+
+// tableau d'object en javascript
+var tabobj = [
+    {nom: 'khady'},
+    {prenom: 'mbaye'},
+    {ville: 'kaffrine'},
+    {age: 79}
+
+]
+console.log(tabobj)
+console.log(' ')
